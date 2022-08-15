445e975dc63d7d85f05b157664a92e8195b3f9d5
unknown <craxwar@gmail.com>
2022-08-15 13:41:21 +0300

    simple todo list

---
+++ To-Do list/app.js
@@ -0,0 +1,43 @@
+function taskList() {
+    document.getElementById('taskBtn').addEventListener('click', onSubmit);
+
+    function onSubmit() {
+        let textField = document.getElementById('tasks');
+
+        if (textField.value == "") {
+            return alert('The text field is required');
+        }
+
+        const ulElement = document.querySelector('ul');
+
+        let liElement = document.createElement('li');
+        liElement.textContent = textField.value;
+
+        let delBtn = document.createElement('button');
+        delBtn.textContent = 'delete';
+        delBtn.addEventListener('click', onDelete);
+        let doneBtn = document.createElement('button');
+        doneBtn.textContent = 'complete';
+        doneBtn.addEventListener('click', onComplete);
+
+        liElement.appendChild(delBtn);
+        liElement.appendChild(doneBtn);
+
+        ulElement.appendChild(liElement);
+
+        textField.value = "";
+
+        function onDelete() {
+            liElement.remove();
+        }
+        
+        function onComplete(e) {
+            let completedTasks = document.getElementById('complete');
+            let task = e.currentTarget.parentNode
+            e.currentTarget.remove();
+            delBtn.textContent = 'clear'
+            completedTasks.appendChild(task);
+        }
+
+    }
+}
