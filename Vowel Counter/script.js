d81215c17320505857cdfd512f0d4f23e4ac08d8
unknown <craxwar@gmail.com>
2022-09-19 15:37:10 +0300

    Removed alert and added writing over page

--- Vowel Counter/script.js
+++ Vowel Counter/script.js
@@ -1,26 +1,47 @@
 function vowelCounter() {
-    const vowels = ["a", "e", "i", "o", "u"];
-
     const btn = document.querySelector('button');
     btn.addEventListener('click', onClick);
 
-    function onClick() {
-        let counter = 0;
+}
 
-        const word = document.querySelector('input').value;
+function onClick() {
+    const vowels = ["a", "e", "i", "o", "u"];
 
-        if (word.length == 0) {
-            return alert('Please write a word!');
-        }
+    const bodyEl = document.querySelector('body');
+    const word = document.querySelector('input').value;
 
-        for (const letters of word) {
-            if (vowels.includes(letters)) {
-                counter++;
-            }
-        }
-        if (counter > 0) {
-            return alert(`The word has ${counter} vowels.`);
-        }
-        return alert('The word has no vowels.');
+    let counter = 0;
+
+    if (word.length == 0) {
+        return bodyEl.innerHTML = `
+        <h1>Please write a word!</h1>
+        <button onclick="onReturn()">Check next word.</button>`;
     }
+
+    for (const letters of word) {
+        if (vowels.includes(letters)) {
+            counter++;
+        }
+    }
+
+    if (counter > 0) {
+        return bodyEl.innerHTML = `
+        <h1>The word has ${counter} vowels.</h1>
+        <button id="returnBtn" onclick="onReturn()">Check next word.</button>`
+    }
+
+    return bodyEl.innerHTML = `
+    <h1>The word has no vowels.</h1>
+    <button onclick="onReturn()">Check next word.</button>`
+
+}
+
+function onReturn() {
+    const bodyEl = document.querySelector('body');
+
+    bodyEl.innerHTML = `
+    <h1>Vowel Counter</h1>
+    <input type="text" placeholder="Word">
+    <button onclick="onClick()">Count</button>`;
+
 }
